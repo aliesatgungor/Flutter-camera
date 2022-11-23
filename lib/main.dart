9b74ab7e0a689dd5bb691bbe3c3e8abcd1f452e4
aliesatgungor <aliesatgungor@outlook.com.tr>
2022-11-23 22:25:43 +0300

    shutter speed is now changeable

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,9 @@ class _CameraAppState extends State<CameraApp> {
     super.initState();
     controller = CameraController(
       cameras[1],
-      ResolutionPreset.medium,
-      iso: 10,
-      shutterSpeed: 800,
+      ResolutionPreset.ultraHigh,
+      iso: 200,
+      shutterSpeed: 1000,
       whiteBalance: WhiteBalancePreset.cloudy,
       focusDistance: 0.1,
     );
@@ -35,28 +35,129 @@ class _CameraAppState extends State<CameraApp> {
         return;
       }
       setState(() {});
+      Future.delayed(Duration(
+        milliseconds: 1000,
+      ));
     });
   }
 
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
     return MaterialApp(
-        title: 'App name',
+        title: 'MartilarTK',
         home: Scaffold(
             appBar: AppBar(
-              title: Text("FOR UMUT KOCABAŞ"),
+              title: const Text("For Martilartk"),
             ),
-            body: AspectRatio(
-                aspectRatio: controller.value.aspectRatio,
-                child: CameraPreview(controller))));
+            body: SingleChildScrollView(
+                child: Column(children: [
+              AspectRatio(
+                  aspectRatio: controller.value.aspectRatio,
+                  child: CameraPreview(controller)),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.shutterSpeed = controller.shutterSpeed + 50;
+                      controller.initialize().then((_) {
+                        if (!mounted) {
+                          return;
+                        }
+                        setState(() {});
+                        Future.delayed(Duration(
+                          milliseconds: 1000,
+                        ));
+                      });
+                    },
+                    child: const Text("Shutter Arttır"),
+                    style: TextButton.styleFrom(
+                      minimumSize: const Size(30, 7),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0)),
+                      backgroundColor: const Color(0xFF00c8ff),
+                      surfaceTintColor: const Color(0x0f000000),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.shutterSpeed = controller.shutterSpeed - 50;
+                      controller.initialize().then((_) {
+                        if (!mounted) {
+                          return;
+                        }
+                        setState(() {});
+                        Future.delayed(Duration(
+                          milliseconds: 1000,
+                        ));
+                      });
+                    },
+                    child: const Text("Shutter Azalt"),
+                    style: TextButton.styleFrom(
+                      minimumSize: const Size(30, 7),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0)),
+                      backgroundColor: const Color(0xFF00c8ff),
+                      surfaceTintColor: const Color(0x0f000000),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.iso = controller.iso + 20;
+
+                      controller.initialize().then((_) {
+                        if (!mounted) {
+                          return;
+                        }
+                        setState(() {});
+                        Future.delayed(Duration(
+                          milliseconds: 1000,
+                        ));
+                      });
+                    },
+                    child: const Text("iso Arttır"),
+                    style: TextButton.styleFrom(
+                      minimumSize: const Size(30, 7),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0)),
+                      backgroundColor: const Color(0xFF00c8ff),
+                      surfaceTintColor: const Color(0x0f000000),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      controller.iso = controller.iso - 20;
+
+                      controller.initialize().then((_) {
+                        if (!mounted) {
+                          return;
+                        }
+                        setState(() {});
+                        Future.delayed(Duration(
+                          milliseconds: 1000,
+                        ));
+                      });
+                    },
+                    child: const Text("iso Azalt"),
+                    style: TextButton.styleFrom(
+                      minimumSize: const Size(30, 7),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0)),
+                      backgroundColor: const Color(0xFF00c8ff),
+                      surfaceTintColor: const Color(0x0f000000),
+                    ),
+                  ),
+                ],
+              ),
+            ]))));
   }
 }
